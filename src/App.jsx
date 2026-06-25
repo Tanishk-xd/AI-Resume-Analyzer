70d235f2c98b26f0c5654dfb57b00f357c7f9c3e
Tanishk Tiwari <tiwaritanishk4@gmail.com>
2026-06-25 18:58:23 +0530

    Day 1: Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>🤖 AI Resume Analyzer</h1>
+
+      <p className="subtitle">
+        Upload your resume and compare it with any Job Description using AI.
+      </p>
+
+      <button className="upload-btn">
+        Upload Resume
+      </button>
+    </div>
+  );
+}
+
+export default App;
